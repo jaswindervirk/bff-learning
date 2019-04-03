2633fd4ef3300953682f9664584420276ce99e3d
oyo <oyo@Administrators-MacBook-Pro-51.local>
2019-04-03 17:38:20 +0530

    second commit

--- Bff-Core/src/main/java/com/oyo/consumerbff/bffcore/controllers/QuoteController.java
+++ Bff-Core/src/main/java/com/oyo/consumerbff/bffcore/controllers/QuoteController.java
@@ -1,5 +1,26 @@
 package com.oyo.consumerbff.bffcore.controllers;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.util.HashMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class QuoteController {
 
+  private static Logger logger= LoggerFactory.getLogger("logstash");
+  private static Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+  @RequestMapping(value ="/check",method = RequestMethod.GET)
+  public String fun()
+  {
+    HashMap<String,Object> hashMap=new HashMap<>();
+    hashMap.put("NameAsli","Jaswinder");
+    hashMap.put("Married",false);
+    logger.warn("{}",gson.toJson(hashMap));
+    return "hi";
+  }
 }
